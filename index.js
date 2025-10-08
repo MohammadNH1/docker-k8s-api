4da5c16ea11dd37a06f1630fb942073782e1ca6e
MohammadNH1 <mustafasheikh28864@gmail.com>
2025-10-08 13:25:00 +0600

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({message:'Hello From Docker and Kubernetes!'})
+})
+
+
+app.listen(3001,()=>{
+    console.log('server listen on port 3001')
+})
