35ba407b5c859cf21fa812b47a6ba0ab9ed58c80
MohammadNH1 <mustafasheikh28864@gmail.com>
2025-10-08 14:35:58 +0600

    update docker hub

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.get('/',(req,res)=>{
     return res.status(200).json({message:'Hello From Docker and Kubernetes!'})
 })
 
-
 app.listen(3001,()=>{
     console.log('server listen on port 3001')
 })
